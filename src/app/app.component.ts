7974d4556602701182f732b2e11a59a1f8b37f18
diniesyahirah <dinie.syahirah@gofreightz.com>
2023-03-23 13:14:57 +0800

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+// import commentData from ''
+import commentData from './data.json'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'interactive-comment';
+  listData = commentData;
+  commentList : any = [];
+
+  constructor(private router: Router,) { }
+
+  ngOnInit(): void {
+    this.commentList = this.listData.comments
+    console.log(this.commentList)
+  }
 }
